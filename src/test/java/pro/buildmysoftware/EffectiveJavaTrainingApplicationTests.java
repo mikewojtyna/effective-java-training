e8b7c54ac9ff5e457b5433352620a76cc7b0e45d
Mike Wojtyna <mike@buildmysoftware.pro>
2019-12-17 00:24:53 +0100

    Init project

---
+++ src/test/java/pro/buildmysoftware/EffectiveJavaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.buildmysoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
